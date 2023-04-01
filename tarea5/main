951e2dfe14bdea0b10774ea905ca86289ef3fc15
mingfanxu <89093374+mingfanxu@users.noreply.github.com>
2023-03-31 19:46:10 -0700

    Delete tarea5 directory

--- tarea5/main
+++
@@ -1,5 +0,0 @@
-#include "src/sdlapp.hpp "
-int main (int argc,char* argv [])
-{
-    return SDLApp::get().on_correr();
-};
--- tarea5/objetos_dinamicos/Entidad.cpp
+++
@@ -1,7 +0,0 @@
-#include "Entidad.hpp"
-#include "../motor/Pipeline.hpp"
-void Entidad::render(SDL_Renderer *r)
-{
-    Pipeline temp(*r);
-    cleatemp.figuras ({avatar});
-} ;
